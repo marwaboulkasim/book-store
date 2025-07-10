902043334343334b163ba4fc2d202c02aba4d732
Marwa <marwa-aboulkasim@hotmail.fr>
2025-07-10 12:27:53 +0200

    maj

--- 3_request_bonus.sql
+++ 3_request_bonus.sql
@@ -2,8 +2,25 @@
 
 -- 1. Trouver les 3 auteurs dont les livres ont généré le plus de chiffre d’affaires total.
 
+SELECT 
+    author.first_name || ' ' || author.last_name AS auteur,
+    SUM(customer_order.total_amount) AS chiffre_affaires
+FROM author
+JOIN book ON author.author_id = book.author_id
+JOIN customer_order ON book.book_id = customer_order.book_id
+GROUP BY author.author_id
+ORDER BY chiffre_affaires DESC
+LIMIT 3;
 
 -- 2. Calculer le total des ventes par thème de livre, classé du plus au moins vendu (en montant).
+ SELECT 
+    theme.theme_name AS theme,
+    SUM(customer_order.total_amount) AS total_ventes
+FROM theme
+JOIN book ON theme.theme_id = book.theme_id
+JOIN customer_order ON book.book_id = customer_order.book_id
+GROUP BY theme.theme_id
+ORDER BY total_ventes DESC;
 
 
 -- 3. Afficher pour chaque mois de l’année 2024 le nombre de commandes passées et le chiffre d’affaires total.
